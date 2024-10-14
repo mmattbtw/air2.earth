ba3e0e1d910c4fe3e376dacdca2a0eeb041c74ce
mmattbtw <matt@mmatt.net>
2024-10-14 13:33:38 -0500

    base

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,23 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  return rss({
+    title: "air2.earth",
+    description: "the informal blog space companion from mmatt.net",
+    site: context.site,
+    items: posts.map((post) => ({
+      link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+      title: post.title,
+      description: post.description,
+      pubDate: post.pubDate,
+    })),
+  });
 }
